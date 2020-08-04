2b3f90344597500adcbc0357eae40b28b0e27975
John Foster <john44foster@aol.com>
2020-08-04 13:13:12 +0100

    Added Burger Menu to SKELETON 1
    +Responsive Burger Menu

--- Website-Skeleton/script.js
+++ Website-Skeleton/script.js
@@ -1,4 +1,36 @@
+// in sticky
+var header = document.querySelector("header");
+// in burger
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
+
+
+// ---------------- sticky nav ------------------------
 window.addEventListener("scroll", function () {
-  var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
+  burger.classList.toggle('sticky', window.scrollY > 0);
 });
+
+// ------------------- burger nav ---------------------
+const navSlide = () => {
+  //   toggle the nav burger
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //   animate the links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    // burger anim
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
