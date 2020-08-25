4b4dd0219ea39f29d743a81543219fbcd5bd7cb4
John Foster <john44foster@aol.com>
2020-08-25 17:21:07 +0100

    Riah Project Start
    Starting getting the main features of the website. Design is to be like an old Windows 3.x desktop. Links open moveable 'windows'.
    
    +windows that drag tester
    +nav bar basic
    +drop down menu

---
+++ RiahProject/resources/script.js
@@ -0,0 +1,53 @@
+document.getElementById("hide-button").addEventListener("click", hide);
+document.getElementById("show-button").addEventListener("click", show);
+
+
+// hiding buttons ------------------------------------------------------------
+
+function hide() {
+    document.getElementById("dragger").classList.add("hidden");
+}
+function show() {
+    document.getElementById("dragger").classList.remove("hidden");
+}
+
+// dragging -------------------------------------------------------------------
+
+dragElement(document.getElementById("dragger"));
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
