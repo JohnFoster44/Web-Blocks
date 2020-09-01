df119dde76ea75dc2a5598416c52e0218e9ecef0
John Foster <john44foster@aol.com>
2020-09-01 18:18:19 +0100

    Update 01/09/20
    +styling inline with windows95
    +interact.js from window header
    
    Still to come:
    dynamic windows based off logo clicked.
    rest of site!

--- RiahProject_draggablejs/resources/script.js
+++ RiahProject_draggablejs/resources/script.js
@@ -1,24 +1,45 @@
+// Nav Button -------------------------------------------------------------------
 
-// interact.js
+function menuButton() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+
+
+// interact.js ---------------------------------------------------------------------
 const position = { x: 0, y: 0 };
 
-interact(".draggable").draggable({
+interact(".draggable").allowFrom('.window-header').draggable({
   // enable inertial throwing
   inertia: true,
   // keep the element within the area of it's parent
   modifiers: [
     interact.modifiers.restrictRect({
       restriction: "parent",
-      endOnly: true,
+      endOnly: false,
     }),
   ],
   // enable autoScroll
-  autoScroll: true,
+  autoScroll: false,
 
   listeners: {
     // call this function on every dragmove event
     move: dragMoveListener,
   },
+
 });
 
 function dragMoveListener(event) {
@@ -34,93 +55,4 @@ function dragMoveListener(event) {
   // update the posiion attributes
   target.setAttribute("data-x", x);
   target.setAttribute("data-y", y);
-
 }
-
-window.dragMoveListener = dragMoveListener;
-// ------------------------
-interact('.resize-drag')
-  .resizable({
-    // resize from all edges and corners
-    edges: { left: true, right: true, bottom: true, top: true },
-
-    listeners: {
-      move (event) {
-        var target = event.target
-        var x = (parseFloat(target.getAttribute('data-x')) || 0)
-        var y = (parseFloat(target.getAttribute('data-y')) || 0)
-
-        // update the element's style
-        target.style.width = event.rect.width + 'px'
-        target.style.height = event.rect.height + 'px'
-
-        // translate when resizing from top or left edges
-        x += event.deltaRect.left
-        y += event.deltaRect.top
-
-        target.style.webkitTransform = target.style.transform =
-          'translate(' + x + 'px,' + y + 'px)'
-
-        target.setAttribute('data-x', x)
-        target.setAttribute('data-y', y)
-        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
-      }
-    },
-    modifiers: [
-      // keep the edges inside the parent
-      interact.modifiers.restrictEdges({
-        outer: 'parent'
-      }),
-
-      // minimum size
-      interact.modifiers.restrictSize({
-        min: { width: 100, height: 50 }
-      })
-    ],
-
-    inertia: true
-  })
-  .draggable({
-    listeners: { move: window.dragMoveListener },
-    inertia: true,
-    modifiers: [
-      interact.modifiers.restrictRect({
-        restriction: 'parent',
-        endOnly: true
-      })
-    ]
-  })
-// interact('.resize').resizable({
-//   edges: {
-//     top: false,
-//     left: false,
-//     bottom: true,
-//     right: true
-//   }
-// }).on{'resizemove', event => {
-//   let {x,y} = event.target.dataset
-
-//   x = parseFloat(x) || 0
-//   y = parseFloat(y) || 0
-
-//   Object.assign(event.target.style, {
-//     width: `${event.rect.width}px`,
-//     height: `${event.rect.height}px`,
-//     transform: `translate(${event.deltaRect.bottom}px, ${event.deltaRect.right}px)`
-//   })
-
-//   Object.assign(event.target.dataset, {x,y})
-  
-// }}
-// ------------------------
-// document.getElementById("hide-button").addEventListener("click", hide);
-// document.getElementById("icon-container").addEventListener("click", show);
-
-// // hiding buttons
-
-// function hide() {
-//   document.getElementById("").classList.add("hidden");
-// }
-// function show() {
-//   document.getElementById("").classList.remove("hidden");
-// }
