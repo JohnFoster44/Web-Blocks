880db71bbde21e9d90ecd175734cb7245007a739
John Foster <john44foster@aol.com>
2020-09-03 13:06:10 +0100

    03/09/20
    Cleaned up the JS and changed the icons. Styling changes for cursor interaction.

--- RiahProject_draggablejs/resources/script.js
+++ RiahProject_draggablejs/resources/script.js
@@ -1,46 +1,46 @@
 // Nav Button -------------------------------------------------------------------
+var dropButton = document.querySelector('.dropbtn');
 
-function menuButton() {
+dropButton.addEventListener('click', function menuButton() {
   document.getElementById("myDropdown").classList.toggle("show");
-}
-
+});
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    var dropdowns = document.querySelectorAll(".dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-} 
-
+};
 
 // interact.js ---------------------------------------------------------------------
 const position = { x: 0, y: 0 };
 
-interact(".draggable").allowFrom('.window-header').draggable({
-  // enable inertial throwing
-  inertia: true,
-  // keep the element within the area of it's parent
-  modifiers: [
-    interact.modifiers.restrictRect({
-      restriction: "parent",
-      endOnly: false,
-    }),
-  ],
-  // enable autoScroll
-  autoScroll: false,
+interact(".draggable")
+  .allowFrom(".window-header")
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: "parent",
+        endOnly: false,
+      }),
+    ],
+    // enable autoScroll
+    autoScroll: false,
 
-  listeners: {
-    // call this function on every dragmove event
-    move: dragMoveListener,
-  },
-
-});
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+    },
+  });
 
 function dragMoveListener(event) {
   var target = event.target;
